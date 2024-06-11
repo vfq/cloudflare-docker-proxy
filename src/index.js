9102e2fbba823e86c3d9a6c49cdc41a5fbd041df
Feature <33208717+vfq@users.noreply.github.com>
2024-06-11 16:40:54 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.many.games": "https://registry-1.docker.io",
+  "quay.many.games": "https://quay.io",
+  "gcr.many.games": "https://gcr.io",
+  "k8s-gcr.many.games": "https://k8s.gcr.io",
+  "k8s.many.games": "https://registry.k8s.io",
+  "ghcr.many.games": "https://ghcr.io",
+  "cloudsmith.many.games": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
